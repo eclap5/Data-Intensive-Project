a7c83486c02df8e4403f5afda32190bffb6c1203
Aleksi Haapalainen <aleksi.haapalainen@gmail.com>
2024-12-02 11:29:24 +0200

    added start script

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-import path from "path";
 import router from "./routes/index.js";
 
 const app = express();
@@ -11,9 +10,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 app.use('/', router);
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
